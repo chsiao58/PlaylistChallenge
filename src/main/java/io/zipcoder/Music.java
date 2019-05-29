20ba1f93582b6dd9694a3d0f7e3605d5d6bab6b7
Joanna Hsiao <chsiao58@gmail.com>
2019-05-29 14:06:17 -0400

    Test passed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,39 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        Integer downCount = countSelectDown(startIndex,selection);
+        Integer upCount = countSelectUp(startIndex,selection);
+        if (downCount<=upCount)
+            return downCount;
+        else
+            return upCount;
+    }
+
+    private Integer countSelectDown(Integer startIndex, String selection)
+    {
+        Integer count = 0;
+        Integer index = startIndex;
+        while(!playList[index].equals(selection)) {
+            index++;
+            count++;
+            if (index >= playList.length)
+                index = 0;
+        }
+        return count;
+    }
+
+    private Integer countSelectUp(Integer startIndex, String selection)
+    {
+        Integer count = 0;
+        Integer index = startIndex;
+        while(!playList[index].equals(selection)) {
+            index--;
+            count++;
+            if (index < 0)
+                index = playList.length-1;
+        }
+        return count;
     }
 }
+
